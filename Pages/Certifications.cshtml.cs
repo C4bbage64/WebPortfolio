a70f49bf2a20c70f0fb5ad40892f4952e30c07ae
Muhammad Irfan <m.irfandeni@gmail.com>
2025-07-17 12:22:11 +0800

    Add project files.

---
+++ Pages/Certifications.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebPortfolio.Pages
+{
+    public class CertificationsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
